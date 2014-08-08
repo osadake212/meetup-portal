357cd1d4d579008487c492e09b0206b0f131e88e
Osada Takuya <osadake.212.dev@gmail.com>
2014-08-08 18:01:30 +0900

    modify before action for user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
