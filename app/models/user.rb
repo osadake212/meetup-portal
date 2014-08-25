6d88d1a26e368c3ee16a7a1cef2d65ae5e6ff366
Osada Takuya <osadake.212.dev@gmail.com>
2014-08-25 12:32:43 +0900

    create relationships model for following-users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :attendances, dependent: :destroy
   has_many :attend_meetups, through: :attendances, source: :meetup
   has_many :microposts
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
 
   before_save { self.email = email.downcase }
   before_create :create_remember_roken
---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Relationship do
+
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  it { should respond_to(:follower_id) }
+  it { should respond_to(:followed_id) }
+end
