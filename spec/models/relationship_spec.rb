69ef05462a2f8ecb0cd1bb8c5b24036e0c226b81
Osada Takuya <osadake.212.dev@gmail.com>
2014-08-28 09:01:20 +0900

    modify relationship model's spec.

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -12,4 +12,16 @@ describe Relationship do
 
   it { should respond_to(:follower_id) }
   it { should respond_to(:followed_id) }
+  it { relationship.followed_id.should eq followed.id }
+  it { relationship.follower_id.should eq follower.id }
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not prenset" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
